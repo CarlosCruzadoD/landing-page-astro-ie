871e8613b6e53a1c2c2007ad582cced3b1ed2bed
carloscd <carlos17cruzado@gmail.com>
2025-03-10 17:37:23 -0500

    workflows 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://CarlosCruzadoD.github.io',
+  site: 'https://carloscruzadod.github.io',
   base: 'landing-page-astro-ie',
   vite: {
     plugins: [tailwindcss()]
